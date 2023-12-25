a18f59c6e47376199cbdf2f3e67f2f5f0f47a407
Mohsen <mohsn.mirzaei@gmail.com>
2023-12-25 13:06:07 +0330

    Handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { Controller, useForm } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 interface IssueForm {
   title: string;
@@ -15,26 +16,43 @@ interface IssueForm {
 const NewIssuePage = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
+
   return (
-    <form
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-      className="max-w-xl space-y-3"
-    >
-      <TextField.Root>
-        <TextField.Input placeholder="موضوع" {...register("title")} />
-      </TextField.Root>
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE className="text-right" placeholder="توضیحات" {...field} />
-        )}
-      />
-      <Button>ثبت مسئله جدید</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("یک خطای غیرمنتظره رخ داد!");
+          }
+        })}
+        className="space-y-3"
+      >
+        <TextField.Root>
+          <TextField.Input placeholder="موضوع" {...register("title")} />
+        </TextField.Root>
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE
+              className="text-right"
+              placeholder="توضیحات"
+              {...field}
+            />
+          )}
+        />
+        <Button>ثبت مسئله جدید</Button>
+      </form>
+    </div>
   );
 };
 
