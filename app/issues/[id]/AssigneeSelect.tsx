6aaa345482649a49631b29b188056800ee108b6c
Mohsen <mohsn.mirzaei@gmail.com>
2024-01-17 09:32:51 +0330

    Populate the assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,32 @@
 "use client";
 
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users");
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="اختصاص دادن به..." dir="ltr" />
       <Select.Content>
         <Select.Group>
           <Select.Label>پیشنهادات</Select.Label>
-          <Select.Item value="1">Mohsen Mirzaei</Select.Item>
+          {users.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
